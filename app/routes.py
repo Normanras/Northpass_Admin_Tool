e81b9e2c14534ca061e21a5ce47e07574810cf4e
Norm Rasmussen <norman.rasmussen@me.com>
2023-02-21 16:59:41 -0500

    Added functions and grid squares. Good progress! Need to figure out how to get the session key into the javascript file.

--- app/routes.py
+++ app/routes.py
@@ -8,61 +8,126 @@ import re
 
 @app.route("/", methods=["GET", "POST"])
 def ask_key():
+    session.clear()
     if request.method == "POST":
-        session['key'] = request.form.get('apikey')
-        print(session['key'])
-        flash(session['key'])
-        if re.search(r'(\S+\w+)', session['key']) and len(session['key']) > 5:
-            print("regex worked")
-            print(len(session['key']))
-            return render_template("get.html", title="Options")
+        session["key"] = request.form.get("apikey")
+        if re.search("\s", session["key"]):
+            error = "Hm. That doesn't seem right"
+            return render_template("index.html", title="Home", error=error)
+        elif session["key"] is not None and len(session["key"]) > 10:
+            return render_template("options.html", title="Options")
         else:
             error = "Hm. That doesn't seem right"
             return render_template("index.html", title="Home", error=error)
+
     else:
         return render_template("index.html", title="Home")
+
+
+@app.route("/", methods=["GET", "POST"])
+def render_home():
+    return render_template("index.html", title="Home")
 
 
 @app.route("/get_courses", methods=["GET", "POST"])
 def get_courses():
     array = []
     df = pd.DataFrame()
+    tempdf = pd.DataFrame()
+    pd.set_option("display.max_colwidth", 100)
+    x = 0
+
     if request.method == "POST":
-        url = "https://api.northpass.com/v2/courses"
-        headers = {"accept": "application/json", "X-Api-Key": session['key']}
-        response = requests.get(url, headers=headers)
-        jsonresponse = response.json()
-        dt = jsonresponse["data"]
-        for course in dt:
-            df = df.append(course["attributes"], ignore_index=True)
+        while True:
+            x += 1
+            url = f"https://api.northpass.com/v2/courses?page={x}"
+            headers = {"accept": "application/json", "X-Api-Key": session["key"]}
+            response = requests.get(url, headers=headers)
+            jsonresponse = response.json()
+            dt = jsonresponse["data"]
+            next = jsonresponse["links"]
+
+            for course in dt:
+                df = df.append(course["attributes"], ignore_index=True)
+                df = df.drop("list_image_url", axis=1)
+
+            if "next" not in next:
+                break
+
+        dfhtml = df.to_html(col_space=5)
+        session["dfcsv"] = df.to_csv()
+        return render_template("table.html", tables=dfhtml, titles="Course List")
+
     else:
         return "This isn't working. Let's go our own way."
-    print(df)
-    download = make_response(df.to_csv())
-    download.headers["Content-Disposition"] = "attachment; filename=export.csv"
-    download.headers["Content-Type"] = "text/csv"
-    return download
+
+
+@app.route("/table")
+def table():
+    return render_template("table.html", tables=[session["dfhtml"]], titles=["Table"])
+
+
+@app.route("/downloadcsv", methods=["GET", "POST"])
+def download_csv():
+    if request.method == "GET":
+        download = make_response(session["dfcsv"])
+        download.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        download.headers["Content-Type"] = "text/csv"
+        return download
 
 
 @app.route("/get_people", methods=["GET", "POST"])
 def get_people():
-    print(session['key'])
+    array = []
+    df = pd.DataFrame()
+    x = 0
+
     if request.method == "POST":
-        url = "https://api.northpass.com/v2/people"
-        headers = {"accept": "application/json", "X-Api-Key": session['key']}
-        response = requests.get(url, headers=headers)
-        jsonresponse = response.json()
-        dt = jsonresponse
-        person = dt
-        print(person)
-        return person
+        while True:
+            x += 1
+            url = f"https://api.northpass.com/v2/people?page={x}"
+            headers = {"accept": "application/json", "X-Api-Key": session["key"]}
+            response = requests.get(url, headers=headers)
+            jsonresponse = response.json()
+            dt = jsonresponse["data"]
+            next = jsonresponse["links"]
+
+            for person in dt:
+                print(person)
+                df = df.append(person["attributes"], ignore_index=True)
+
+            if "next" not in next:
+                break
+
+        dfppl = df.to_html(col_space=5)
+        session["dfcsv"] = df.to_csv()
+        return render_template("table.html", tables=dfppl, titles="People List")
+
     else:
         return "what what"
 
 
-@app.route("/startpage", methods=["GET", "POST"])
-def startpage():
-    return render_template("startpage.html", title="StartTest")
+@app.route("/add_options")
+def add_options():
+    return render_template("bulk_add.html", title="Bulk Add Learners")
+
+
+@app.route("/bulk_add", methods=["GET", "POST"])
+def bulk_add():
+    if request.method == "POST":
+        emails = request.form.get("emails")
+        groups = request.form.get("groups")
+        url = "https://api.northpass.com/v2/bulk/people"
+
+        payload = {
+            "data": {"attributes": {"people": [{"email": emails, "groups": groups}]}}
+        }
+
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-Api-Key": session["key"],
+        }
 
 
 app.secret_key = "@&I\x1a?\xce\x94\xbb0w\x17\xbf&Y\xa2\xc2(A\xf5\xf2\x97\xba\xeb\xfa"
